b38e5b72d5af21cacba593a4ec805ee909a63c60
Carlos Eduardo Ramirez Martinez <c.ramirezm@uniandes.edu.co>
2022-09-14 15:22:42 -0500

    Creacion relacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AerolineaModule } from './aerolinea/aerolinea.module';
 import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
+import { AerolineaAeropuertoModule } from './aerolinea-aeropuerto/aerolinea-aeropuerto.module';
 
 @Module({
-  imports: [AerolineaModule, AeropuertoModule],
+  imports: [AerolineaModule, AeropuertoModule, AerolineaAeropuertoModule],
   controllers: [AppController],
   providers: [AppService],
 })
